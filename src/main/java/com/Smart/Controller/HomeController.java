8e3eb4d606b42d3940881b76faaa86a0a0723fc8
piyush2040 <51694158+piyush2040@users.noreply.github.com>
2024-07-31 23:17:21 +0530

    adding thymeleaf templete

--- src/main/java/com/Smart/Controller/HomeController.java
+++ src/main/java/com/Smart/Controller/HomeController.java
@@ -1,5 +1,33 @@
 package com.Smart.Controller;
 
-public class HomeController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.Smart.dao.UserRepository;
+import com.Smart.entities.User;
+
+@Controller
+public class HomeController {
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("/test")
+	@ResponseBody
+	public String test()
+	{
+		User user = new User();
+		user.setName("Piyush");
+		user.setEmail("piyush@gmail.com");
+		userRepository.save(user);
+				
+		return "working";
+	}
+	@RequestMapping("/home")
+	public String home()
+	{
+		return "home";
+	}
 }
--- src/main/java/com/Smart/entities/Contact.java
+++ src/main/java/com/Smart/entities/Contact.java
@@ -1,7 +1,5 @@
 package com.Smart.entities;
 
-import org.hibernate.annotations.ManyToAny;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;

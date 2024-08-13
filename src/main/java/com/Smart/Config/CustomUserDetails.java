c256d5a6a1e14b03db5dd9fd9c712fc932229162
piyush2040 <51694158+piyush2040@users.noreply.github.com>
2024-08-13 23:16:55 +0530

    Implementation of userDetails and userDetailService

--- src/main/java/com/Smart/Config/CustomUserDetails.java
+++ src/main/java/com/Smart/Config/CustomUserDetails.java
@@ -1,5 +1,41 @@
 package com.Smart.Config;
 
-public class CustomUserDetails {
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.Smart.dao.UserRepository;
+import com.Smart.entities.User;
+
+public class CustomUserDetails implements UserDetails{
+
+	private User user;
+	
+	public CustomUserDetails(User user) {
+		super();
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		SimpleGrantedAuthority simpleGrantedAuthority =  new SimpleGrantedAuthority(user.getRole());
+		return List.of(simpleGrantedAuthority);
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return user.getEmail();
+	}
 
 }
--- src/main/java/com/Smart/Config/UserDetailsServiceImpl.java
+++ src/main/java/com/Smart/Config/UserDetailsServiceImpl.java
@@ -1,5 +1,28 @@
 package com.Smart.Config;
 
-public class UserDetailsServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.Smart.dao.UserRepository;
+import com.Smart.entities.User;
+
+public class UserDetailsServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		User user = userRepository.getUserByUserName(username);
+		if(user == null)
+		{
+			throw new UsernameNotFoundException("could note finds user !!");
+		}
+		CustomUserDetails customUserDetails = new CustomUserDetails(user);
+		return customUserDetails;
+	}
 
 }

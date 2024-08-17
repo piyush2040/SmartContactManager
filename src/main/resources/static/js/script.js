f069f9098d69e7d9a453366e135fd3e6369e26e4
piyush2040 <51694158+piyush2040@users.noreply.github.com>
2024-08-17 10:34:42 +0530

    resolved js not rendering bug.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -7,9 +7,11 @@ const toggleSidebar = () => {
 		{
 			$(".sidebar").css("display","none");
 			$(".content").css("margin-left","0%");
+			$("#SideBarBars").css("display","block");
 		}
 		else{
 			$(".sidebar").css("display","block");
 			$(".content").css("margin-left","20%");
+			$("#SideBarBars").css("display","none");
 		}
 }
